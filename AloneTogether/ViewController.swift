f9d42d57c9d9636b0bc120c349f949c7e2943a42
Olly Cohen <olly.k.cohen@gmail.com>
2020-11-15 16:58:38 -0600

    Initial Commit

---
+++ AloneTogether/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AloneTogether
+//
+//  Created by Oliver K Cohen on 11/15/20.
+//  Copyright © 2020 Oliver K Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
