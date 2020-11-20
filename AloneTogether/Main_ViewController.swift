c245cf2d88dbf5547c903ecd5f0693283ea2bff3
Benjamin Faber <bfaber@wustl.edu>
2020-11-20 17:19:34 -0600

    Friday at 5:20 - B Storyboard with auto-layout complete first commit View Controller files (with some outlets)

---
+++ AloneTogether/Main_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Main_ViewController.swift
+//  AloneTogether
+//
+//  Created by Benjamin Faber on 11/20/20.
+//  Copyright © 2020 Oliver K Cohen. All rights reserved.
+//
+
+import UIKit
+
+class Main_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
