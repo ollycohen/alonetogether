c4a5c211b8db44dfd047dc619b6004a302282e8b
Benjamin Faber <bfaber@wustl.edu>
2020-11-21 19:04:59 -0600

    First Commit Test

--- AloneTogether/ViewController.swift
+++ AloneTogether/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // This is the app - Olly
+        // First Commit - Benjamin
     }
 
 
