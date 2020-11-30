f2ff52bdc1dc359abc8d5fe19bcb6f9b9102e938
Benjamin Faber <bfaber@wustl.edu>
2020-11-29 19:13:07 -0600

    Front end updated

---
+++ AloneTogether/C/RegisterUserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterUserViewController.swift
+//  AloneTogether
+//
+//  Created by Benjamin Faber on 11/29/20.
+//  Copyright © 2020 Oliver K Cohen. All rights reserved.
+//
+
+import UIKit
+
+class RegisterUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
